b263445f462cc0d7256bc0a356794d661c36c32f
Achraf Mkaouar <59176972+mkaouar-prog@users.noreply.github.com>
2024-11-01 18:10:53 +0100

    Update Projects.js

--- src/components/Projects/Projects.js
+++ src/components/Projects/Projects.js
@@ -67,7 +67,7 @@ function Projects() {
               title="Dalilaltelmidh App"
               description="I developed an Android app using Flutter, focused on providing educational content and interactive learning experiences. The app features user-friendly navigation, multimedia resources . Additionally, I integrated AdMob ads to generate revenue while maintaining a seamless user experience. I also deployed the app on the Google Play Store, making it accessible to a broader audience."
               //ghLink="https://github.com/soumyajit4419/Editor.io"
-              demoLink="https://apkcombo.com/%D8%B4%D8%B1%D8%AD-%D9%86%D8%B5-%D8%AF%D9%84%D9%8A%D9%84-%D8%A7%D9%84%D8%AA%D9%84%D9%85%D9%8A%D8%B0/com.daliltelmidh.dalil_telidh/"              
+              demoLink="https://play.google.com/store/apps/details?id=mobile.charh.nas"              
             />
           </Col>
 
